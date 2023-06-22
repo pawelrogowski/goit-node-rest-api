ee41e2551b78e3590a2c35ce22f22cbbbe36f9a4
Feiron <pawel.rogowski.praca@gmail.com>
2023-06-22 14:08:27 +0200

    added auto creation of ./tmp

--- middlewares/avatarUploadMiddleware.js
+++ middlewares/avatarUploadMiddleware.js
@@ -1,8 +1,13 @@
 const multer = require("multer");
 const path = require("path");
+const fs = require("fs");
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
+    const tmpFolderPath = "./tmp";
+    if (!fs.existsSync(tmpFolderPath)) {
+      fs.mkdirSync(tmpFolderPath);
+    }
     cb(null, "./tmp");
   },
   filename: function (req, file, cb) {

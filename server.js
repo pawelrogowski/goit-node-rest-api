f6f25812e6b0f20b9358d842002a45ea3480a45e
= <=>
2023-07-14 11:21:11 +0200

    removed default port

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const app = require("./app");
 const mongoose = require("mongoose");
 
-const Port = process.env.PORT || 3000;
+const Port = process.env.PORT;
 
 mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
